59e532bf78263a975167377a93d3c947561933d2
nycolle <nycollenp2005@gmail.com>
2024-06-05 02:07:57 -0300

    salvo5

---
+++ tecnologias/indes.js
@@ -0,0 +1,17 @@
+/* tranformar o menu hamburguer em x */
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block'
+
+    } else {
+        nav.style.display = 'none'
+    }
+}
